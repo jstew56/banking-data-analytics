22b29558feeb3637586efba9c58f8dd160a9bb1a
Jessica Stewart <jsbooking56@gmail.com>
2026-04-26 13:53:43 -0400

    Update data_exploration.sql

--- sql/data_exploration.sql
+++ sql/data_exploration.sql
@@ -1,1 +1,12 @@
+/* ============================================================
+Project: Banking Data Analysis
+Tool: Snowflake SQL
+Author: Jessica Stewart
 
+Business Question:
+What is the average credit card balance owed by customers?
+============================================================ */
+
+SELECT
+    AVG(credit_card_balance) AS avg_balance_owed
+FROM credit;
